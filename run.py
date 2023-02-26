34a0ad51dae5e99647ac9617ddeb521f1a369200
ryan7984 <ryan7984@gmail.com>
2023-02-26 18:55:47 +0000

    Add computer player and board to main game function and add inout for players name

--- run.py
+++ run.py
@@ -23,11 +23,15 @@ def random_col(board):
 
 #Main function to run the game 
 def play_battleships(size):
-    board = create_board(size)
-    print("Ahoy there, let's play Battleships")
-    print_board(board)
-    ship_row = random_row(board)
-    ship_col = random_col(board)
+    player_name = input("What's your name?")
+    player_board = create_board(size)
+    computer_board = create_board(size)
+    print("Ahoy there, let's play Battleships" "Can you sink the computers ships" )
+    print("This is your board:")
+    print_board(player_board)
+    
+    computer_ship_row = random_row(board)
+    computer_ship_col = random_col(board)
 
     for turn in range(4):
         print("Turn", turn + 1)
@@ -38,19 +42,34 @@ def play_battleships(size):
             print("You can only enter a number")
             continue
 
-        if guess_row == ship_row and guess_col == ship_col:
-            print("Fair play to ye! You sank my Battle Ship.")
+        if guess_row == computer_ship_row and guess_col == computer_ship_col:
+            print("Fair play to ye! You sank the computers battle Ship.")
             break
         else:
-            if guess_row not in range(size) or \
-               guess_col not in range(size):
+            if guess_row not in range(size) or guess_col not in range(size):
                print("Way off!, thats not even in the sea!")
-            elif board[guess_row][guess_col] == "X":
+            elif player_board[guess_row][guess_col] == "X":
                print("You guessed that one already")
             else:
-                print("You missed my Battleship! You need to go to specsavers")
-                board[guess_row][guess_col] = "X"
-            print_board(board)
+                print("You missed the computers battleship!")
+                player_board[guess_row][guess_col] = "X"
+            print("Your board:")
+            print_board(player_board)
+            computer_guess_row = random_row(player_board)
+            computer_guess_col = random_col(player_board)
+            if computer_guess_row == computer_ship_row and computer_guess_col == computer_ship_col:
+                print("Oh no! The computer sank your Battleship!")
+                break
+            else:
+                if computer_guess_row not in range(size) or computer_guess_col not in range(size):
+                    print("The computer guessed outside of the board!")
+                elif computer_board[computer_guess_row][computer_guess_col] == "X":
+                    print("The computer already guessed that one!")
+                else:
+                    print("The computer missed!")
+                    computer_board[computer_guess_row][computer_guess_col] = "X"
+            print("The computer's board:")
+            print_board(computer_board)
             if turn == 3:
                 print("Game Over")
 
